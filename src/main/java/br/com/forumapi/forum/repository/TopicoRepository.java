0da7880663ab70eba3f9f3ca943f02a5b76d04a0
Rodrigo Ramos <greff3d@hotmail.com>
2020-08-20 11:14:42 -0300

    Segundo import com add de metodos e cache

--- src/main/java/br/com/forumapi/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/forumapi/forum/repository/TopicoRepository.java
@@ -1,14 +1,14 @@
 package br.com.forumapi.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.forumapi.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 
 
